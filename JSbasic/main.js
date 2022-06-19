7f23da248e63c0a373e2fc534cdf28067cf67d87
NhathaVPS <106251085+NhathaVPS@users.noreply.github.com>
2022-06-19 18:39:36 +0700

    Initial commit

---
+++ JSbasic/main.js
@@ -0,0 +1,9 @@
+/**
+ * if - else
+ */
+var isSuccess = 1 <= 2;
+if(isSuccess){
+    console.log('Điều kiện đúng');
+}else{
+    console.log('Điều kiện sai');
+}
---
+++ T3H/Buồi 4/index13.js
@@ -0,0 +1,1 @@
+console.log('chào JS từ bên ngoài');
